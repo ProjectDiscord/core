d24d7e71dd7395f0d2041b1da7f60cebb90b16fe
Ducko <duckodas@gmail.com>
2025-09-20 14:59:21 +0200

    Add: discordClient

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,31 @@
+import { ObjectNameIDArray } from '@projectdiscord/shared';
 import 'dotenv/config';
 
 interface BotInterface {
 	client_token: string;
 	client_id: string;
 	client_secret: string;
+	client_prefix: string;
 }
 
 export interface ProjectInterface {
-	discordClient: BotInterface;
+	client: BotInterface;
+	guilds: ObjectNameIDArray;
 }
 
 const config: ProjectInterface = {
-	discordClient: {
-		client_token: '',
-		client_id: '',
-		client_secret: '',
+	client: {
+		client_token: process.env.CLIENT_TOKEN as string,
+		client_id: process.env.CLIENT_ID as string,
+		client_secret: process.env.CLIENT_SECRET as string,
+		client_prefix: '?',
 	},
+	guilds: [
+		{
+			name: '',
+			id: '',
+		},
+	],
 };
 
 export default config;

6acd44b744ed78a31a956f58ee7d5755f8214e6b
Ducko <duckodas@gmail.com>
2025-09-20 22:45:59 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,16 @@ interface BotInterface {
 
 export interface ProjectInterface {
 	client: BotInterface;
+	helper: BotInterface;
 	guilds: ObjectNameIDArray;
+	colors: {
+		success: number;
+		error: number;
+		warning: number;
+		info: number;
+		secondary: number;
+		primary: number;
+	};
 }
 
 const config: ProjectInterface = {
@@ -18,14 +27,28 @@ const config: ProjectInterface = {
 		client_token: process.env.CLIENT_TOKEN as string,
 		client_id: process.env.CLIENT_ID as string,
 		client_secret: process.env.CLIENT_SECRET as string,
-		client_prefix: '?',
+		client_prefix: process.env.CLIENT_PREFIX as string,
+	},
+	helper: {
+		client_token: process.env.HELPER_CLIENT_TOKEN as string,
+		client_id: process.env.HELPER_CLIENT_ID as string,
+		client_secret: process.env.HELPER_CLIENT_SECRET as string,
+		client_prefix: process.env.HELPER_CLIENT_PREFIX as string,
 	},
 	guilds: [
 		{
-			name: '',
-			id: '',
+			name: 'Project Discord',
+			id: '1396235829579485214',
 		},
 	],
+	colors: {
+		success: 0x57f287,
+		error: 0xed4245,
+		warning: 0xfaa61a,
+		info: 0x5865f2,
+		secondary: 0x37373d,
+		primary: 0x5865f2,
+	},
 };
 
 export default config;
